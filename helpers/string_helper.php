e916e83a3ececd2ca4719558c5e67523ec61a32a
Caleb Simpson <caleb.simpson@jadedpixel.com>
2013-05-15 16:23:06 -0400

    Add a lambda join string helper.

--- helpers/string_helper.php
+++ helpers/string_helper.php
@@ -5,18 +5,18 @@ class StringHelper
   {
     return strpos(strtolower($haystack), strtolower($needle)) !== false;
   }
-  
+
   static function starts_with($haystack, $needle)
   {
     return !strncmp($haystack, $needle, strlen($needle));
   }
-  
+
   static function ends_with($haystack, $needle)
   {
     $length = strlen($needle);
     if ($length == 0) return true;
     return (substr($haystack, -$length) === $needle);
-  } 
+  }
 
   static function camel_to_underscore($str)
   {
@@ -29,6 +29,18 @@ class StringHelper
     $result = '';
     foreach ($words as $word) {
       $result .= ucfirst(trim($word));
+    }
+    return $result;
+  }
+
+  static function lambda_join($delim, $collection, $lambda)
+  {
+    $result = "";
+    foreach($collection as $element)
+    {
+      $result .= $lambda($element);
+      if ($element !== end($collection))
+        $result .= $delim;
     }
     return $result;
   }

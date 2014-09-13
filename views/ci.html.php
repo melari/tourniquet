406e828daab5d586c327abef8e8f1372b93d3c2a
Caleb Simpson <the.foner@gmail.com>
2014-09-13 10:20:07 -0400

    Display errors when they occur in CI.

--- views/ci.html.php
+++ views/ci.html.php
@@ -1,27 +1,58 @@
-<h3>Tourniquet CI</h3>
+<style type='text/css'>
+  .fail_detail {
+    border: 1px solid red;
+    padding: 10px;
+    margin-bottom: 10px;
+  }
+</style>
 
-<div id="results"></div>
-<hr />
-<div id="details"></div>
-<hr />
-<a href="javascript:show('success_list')">Show full details</a>
-<div id="success_list" style="display:none"></div>
+<div id="content-wrap" style="margin:auto">
+  <div id="content">
+    <h2>Running all tests...</h2>
+
+    <div id="results"></div>
+    <hr />
+    <div id="progress">Waiting on <strong><?= $this->total_count ?></strong> test suites to complete...</div>
+    <hr />
+    <div id="details"></div>
+    <hr />
+    <a href="javascript:show('success_list')">Show full details</a>
+    <div id="success_list" style="display:none"></div>
+  </div>
+</div>
 
 <script type="text/javascript">
   remaining = <?= $this->total_count ?>;
   total         = 0;
   success_count = 0;
   failed_count  = 0;
+  error_count   = 0;
 
   <?php foreach($this->tests as $type => $list) { ?>
     <?php foreach($list as $test) { ?>
       remote_call(url_for("/ci/run/<?= $type ?>/<?= $test ?>.json"), {}, function(result) {
-        report(JSON.parse(result));
+        report(result, "<?= $type ?>", "<?= $test ?>");
       });
     <?php } ?>
   <?php } ?>
 
-  function report(results) {
+  function report(json, type, test) {
+    try {
+      results = JSON.parse(json);
+      results["error"] = 0;
+    } catch(err) {
+      append_html("details", "<div class='fail_detail'><strong> Error occurred while running " + type + " test suite '" + test + "': </strong>" + json + "</div>");
+      append_html("results", "<span style='color:orange'>E</span>");
+      results = {
+        "success": 0,
+        "failure": 0,
+        "error": 1,
+        "details": [],
+        "success_list": []
+      }
+    }
+
+
     new_values = "";
     for(i = 0; i < results["success"]; i++)
       new_values += ".";
@@ -38,11 +69,14 @@
 
     success_count += results["success"];
     failed_count  += results["failure"];
-    total += results["success"] + results["failure"];
+    error_count   += results["error"];
+    total += results["success"] + results["failure"] + results["error"];
 
     remaining--;
     if (remaining == 0) {
-      append_html("results", "<hr />" + total + " TEST COMPLETED | <span style='color:green'>SUCCESS: " + success_count + "</span> | <span style='color:red'>FAILED: " + failed_count + "</span>");
+      set_html("progress", total + " TESTS COMPLETED | <span style='color:green'>SUCCESS: " + success_count + "</span> | <span style='color:red'>FAILED: " + failed_count + "</span> | <span style='color:orange'>ERRORED: " + error_count + "</span>");
+    } else {
+      set_html("progress", "Waiting on <strong>" + remaining + "</strong> test suites to complete...");
     }
   }
 </script>

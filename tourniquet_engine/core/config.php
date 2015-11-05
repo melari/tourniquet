5403591760167622b0d4b5e2280e6b97af1495f4
Caleb Simpson <caleb.simpson@jadedpixel.com>
2015-11-05 13:44:19 -0500

    Allow for users to specify which path to load secrets from.

--- tourniquet_engine/core/config.php
+++ tourniquet_engine/core/config.php
@@ -5,19 +5,24 @@ class Config
   public static $server = "mainline";
   public static $mobile = false;
   public static $app_run_directory = "";
+  public static $secrets_path = "config/secrets.json";
   public static $secrets = array();
 
   /** NOTE: Config::setup should be called AFTER Request::setup **/
   /** Set up server and env variables here... **/
   public static function setup()
   {
-    $raw = file_get_contents(Router::path_for("config/secrets.json"));
+    self::reload_secrets();
+    Router::load_resource('config/config.php');
+  }
+
+  public static function reload_secrets()
+  {
+    $raw = @file_get_contents(Router::path_for(self::$secrets_path));
     if ($raw !== false)
     {
       self::$secrets = json_decode($raw, true);
     }
-
-    Router::load_resource('config/config.php');
   }
 }
 ?>

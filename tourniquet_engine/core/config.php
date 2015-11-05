57efda0ad320572d3b6f0db12d00e722462854dd
Caleb Simpson <caleb.simpson@jadedpixel.com>
2015-11-05 13:30:48 -0500

    Load secrets from config/secrets.json if it exists.

--- tourniquet_engine/core/config.php
+++ tourniquet_engine/core/config.php
@@ -5,11 +5,18 @@ class Config
   public static $server = "mainline";
   public static $mobile = false;
   public static $app_run_directory = "";
+  public static $secrets = array();
 
   /** NOTE: Config::setup should be called AFTER Request::setup **/
   /** Set up server and env variables here... **/
   public static function setup()
   {
+    $raw = file_get_contents(Router::path_for("config/secrets.json"));
+    if ($raw !== false)
+    {
+      self::$secrets = json_decode($raw, true);
+    }
+
     Router::load_resource('config/config.php');
   }
 }
